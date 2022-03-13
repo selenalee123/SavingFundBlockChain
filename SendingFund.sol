2e3a21ace7631f7b7b20d1c108d0997052aea675
Kim Kim <52686208+selenalee123@users.noreply.github.com>
2022-03-13 19:02:50 -0400

    add SendingFund

---
+++ SendingFund.sol
@@ -0,0 +1,22 @@
+﻿// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract OngHeo{
+    //
+    //
+
+  struct sender{
+      address Address;
+      uint Currency;
+  }
+
+  sender[] public Moneysender;
+
+  function sendMoney() public payable{
+    Moneysender.push(sender(msg.sender,msg.value));
+  }
+
+  function checkmoney() public view returns(uint){
+    return address(this).balance;
+  }
+}
